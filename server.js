eecc6d00a45e7a5b4221e9e45cefa54a9ab90321
Teraaret <raiiliiat@yandex.ru>
2017-10-27 11:05:47 +0500

    end?

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 var fs = require('fs');
 
-fs.mkdirSync("test", function(err){
-    if (err && err.code=="EEXIST")
-    {
-        console.log(err);
-    }
-    else
-    {
-        console.log("folder create");
-    }
-});
+//create("i will be renamed");
+//create("i will be deleted");
+//create("i will be live");
+//rename("i will be renamed", "new name");
+//remove("i will be deleted");
 
-fs.renameSync("test","test2", function(e){
-    if (e) { console.log('error rename'); }
-    else { console.log("renamed"); }
-});
+read(".");
 
-fs.rmdirSync("test2", function(e){
-    if (e) { console.log("error") }
-    else { console.log("Papka was deleted") }
-});
+function create(name)
+{
+    fs.mkdirSync(name, function(err){
+        if (err && err.code=="EEXIST")
+        { console.log(err); }
+        else
+        { console.log("folder create"); }
+    });
+}
+
+function rename(name,newname)
+{
+    fs.renameSync(name,newname, function(err){
+        if (err) { console.log('error rename'); }
+        else { console.log("renamed"); }
+    });
+}
+
+function remove(name)
+{
+    fs.rmdirSync(name, function(err){
+        if (err) { console.log("error") }
+        else { console.log("Papka was deleted") }
+    });
+}
+
+function read(name)
+{
+    fs.readdir(name, (err,files)=>{
+        files.forEach(file=>{
+            console.log("- "+file);
+        });
+    });
+}
