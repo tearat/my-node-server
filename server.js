6e23dada61c243da137b9fac0ca362f364495fa1
Teraaret <raiiliiat@yandex.ru>
2017-10-27 11:23:50 +0500

    null

--- server.js
+++ server.js
@@ -1,44 +1,6 @@
 var fs = require('fs');
+var format = require('node.date-time');
 
-//create("i will be renamed");
-//create("i will be deleted");
-//create("i will be live");
-//rename("i will be renamed", "new name");
-//remove("i will be deleted");
-
-read(".");
-
-function create(name)
-{
-    fs.mkdirSync(name, function(err){
-        if (err && err.code=="EEXIST")
-        { console.log(err); }
-        else
-        { console.log("folder create"); }
-    });
-}
-
-function rename(name,newname)
-{
-    fs.renameSync(name,newname, function(err){
-        if (err) { console.log('error rename'); }
-        else { console.log("renamed"); }
-    });
-}
-
-function remove(name)
-{
-    fs.rmdirSync(name, function(err){
-        if (err) { console.log("error") }
-        else { console.log("Papka was deleted") }
-    });
-}
-
-function read(name)
-{
-    fs.readdir(name, (err,files)=>{
-        files.forEach(file=>{
-            console.log("- "+file);
-        });
-    });
-}
+setInterval(function(){
+    fs.appendFile("readme.log", new Date().format("Y-M-d H:M:S")+" "+"hahaha"+"\n");
+},3000);
