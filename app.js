349090cd62c49e64ca86b3c128f5a390a05eeaac
Teraaret <raiiliiat@yandex.ru>
2017-11-13 16:11:10 +0500

    CSS & favicon
    Added separate renderers for CSS and favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var url = require('url');
 var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
 const routes = [
     "/", "/login", "/auth"
@@ -7,37 +8,86 @@ const routes = [
 
 function renderHTML(path, response)
 {
+    console.log("Loading "+path+"...");
+    response.writeHead(200, {'Content-type': 'text/html'});
     var head = fs.readFileSync('./views/_head.html');
     var body = fs.readFileSync(path);
     var end = fs.readFileSync('./views/_end.html');
     var data = head+body+end;
     response.write(data);
-    console.log((path.slice(1,-5)+' Successfully loaded'));
+    console.log((path.slice(1)+' successfully loaded'));
     response.end();
+    
+    console.log();
+}
+
+function renderCSS(path,response)
+{
+    console.log("Loading "+path+"...");
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    var style = fs.readFileSync(path);
+    response.write(style);
+    console.log((path.slice(1)+' successfully loaded'));
+    response.end();
+    
+    console.log();
+}
+
+function renderFavIcon(response)
+{
+    console.log("Loading favicon...");
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    var favicon = fs.readFileSync("./favicon.png");
+    response.write(favicon);
+    console.log('Favicon successfully loaded');
+    response.end();
+    
+    console.log();
 }
 
 module.exports = 
 {
     handleRequest: function(request, response)
     {
-        response.writeHead(200, {'Content-type': 'text/html'});
+        console.log("=== Request sended ===");
+        var mypath = url.parse(request.url).pathname;
+        var extname = path.extname(mypath);
         
-        var path = url.parse(request.url).pathname;
+        // favicon
+        if ( extname == ".ico" )
+        {
+            console.log("Favicon detected");
+            renderFavIcon(response);
+            console.log();
+        }
+        // CSS
+        else if ( extname == ".css" )
+        {
+            console.log("CSS detected: "+mypath);
+            renderCSS("."+mypath,response);
+        }
+        // HTML
+        else
+        {
+            console.log("HTML detected: "+mypath);
+            if ( mypath == "/" )
+            {
+                renderHTML('./views/index.html', response);
+            }
+            else if ( routes.indexOf(mypath) != -1 )
+            {
+                renderHTML('./views'+mypath+'.html', response);
+            }
+            else 
+            {
+                console.log(mypath);
+                console.log("Route not exist (404)");
+                response.writeHead(404);
+                renderHTML('./views/404.html', response);
+            }
+        }
+        // Здесь стоит заметить, что запросы к роуту и css должны происходить в разных процессах
+        // потому что иначе будет write after end
         
-        if ( path == "/" )
-        {
-            renderHTML('./views/index.html', response);
-        }
-        else if ( routes.indexOf(path) != -1 )
-        {
-            renderHTML('./views'+path+'.html', response);
-        }
-        else 
-        {
-            console.log("Route not exist (404)");
-            response.writeHead(404);
-            response.write('404');
-            response.end();
-        }
     }
 };
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var http = require('http');
+var clear = require('clear');
+
 var app = require('./app');
 
 http.createServer(app.handleRequest).listen(8000);
+clear();
 console.log("listen on localhost:8000");
+console.log();
